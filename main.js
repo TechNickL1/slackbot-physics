4baf6d339c3bcbb31f4ed549992539e8cf54fa4a
TechNickL1 <auraceon@gmail.com>
2016-12-12 21:24:20 -0800

    cleanup

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var express = require('express'); //init express
-var app = require('express')(); //init app
+var app = express(); //init app
 var http = require('http').Server(app); //init server
 var io = require('socket.io')(http); //init socket.io
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static("public"));
+app.use(app.static("public"));
 app.use("/bootstrap/css", express.static("node_modules/bootstrap/dist/css"));
 app.use("/bootstrap/js", express.static("node_modules/bootstrap/dist/js"));
 app.use("/jq", express.static("node_modules/jquery/dist"));
