8efeb8f84d9bcc5c2c5114a11ab32031842074c6
TechNickL1 <auraceon@gmail.com>
2017-10-02 14:46:03 -0700

    slash command test

--- main.js
+++ main.js
@@ -6,19 +6,23 @@ var io = require('socket.io')(http); //init socket.io
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
-app.post('/', function (req, res) {
+/*app.post('/', function (req, res) {
   console.log("Incoming POST request:")
   console.log("type = "+req.body.event.type)
   console.log("content = "+req.body.event.text)
   if(req.body.event.type="message"){
     var msg=req.body.event.text
-    var regex = /.*[0-9]+ *ft|feet|foot|in|inch|yard|yd|mi|mile|pound|lb|ton|psi|atm.*/ig;
+    var regex = /.*[0-9]+ *ft|feet|foot|in|inch|yard|yd|mi|mile|pound|lb|ton|psi|atm.*//*ig;
     if(msg === regex){
       console.log("unit found")
     }
   }
+})*/
+app.post('/', function (req, res) {
+  console.log("Incoming POST request:")
+  console.log(req.body)
 })
 
 http.listen(app.get('port'), function(){
