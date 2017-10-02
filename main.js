fc55b29bc5ec7158cc836845ceaf4117973a06c3
TechNickL1 <auraceon@gmail.com>
2017-10-02 11:18:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,20 +4,23 @@ const app = express()
 var http = require('http').Server(app); //init server
 var io = require('socket.io')(http); //init socket.io
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.urlencoded({ extended: false }))
  
 app.post('/', function (req, res) {
   const msg=req.body.Body
   //var regex = /[0-9]+ *ft|feet|foot|in|inch|yard|yd|mi|mile|pound|lb|ton|psi|atm/ig;
   res.set('Content-Type', 'text/plain')
-  res.send(req.challenge)
+  res.send(req.body.challenge)
 })
  
 // Tell our app to listen on port 3000
 app.listen(3000, function (err) {
   if (err) {
     throw err
-  }
+}
  
-  console.log('Server started on port 3000')
-})
+http.listen(app.get('port'), function(){
+  console.log('listening on *:', app.get('port'));
+});
