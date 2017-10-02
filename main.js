bd697cd90711eaa641b12033ef8b8be84f93dbfc
TechNickL1 <auraceon@gmail.com>
2017-10-02 14:13:21 -0700

    test log

--- main.js
+++ main.js
@@ -9,10 +9,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-  //const msg=req.body.Body
-  //var regex = /[0-9]+ *ft|feet|foot|in|inch|yard|yd|mi|mile|pound|lb|ton|psi|atm/ig;
-  console.log(req.body.challenge)
-  res.send(req.body.challenge)
+  if(req.body.type="message"){
+    var msg=req.body.text
+    var regex = /[0-9]+ *ft|feet|foot|in|inch|yard|yd|mi|mile|pound|lb|ton|psi|atm/ig;
+    console.log(req.body.text)
+  }
 })
 
 http.listen(app.get('port'), function(){
