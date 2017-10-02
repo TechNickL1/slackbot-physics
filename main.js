dbed51d784ec781499ce68ffacaab01e11d707c6
TechNickL1 <auraceon@gmail.com>
2017-10-02 14:21:43 -0700

    syntax
    frickin atom

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
   console.log("type= "+req.body.type)
-  console.log("content= "req.body.text)
+  console.log("content= "+req.body.text)
   if(req.body.type="message"){
     var msg=req.body.text
     var regex = /[0-9]+ *ft|feet|foot|in|inch|yard|yd|mi|mile|pound|lb|ton|psi|atm/ig;
