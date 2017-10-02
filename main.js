c0de313a50d664ce1b74c28d56c7c153704e2966
TechNickL1 <auraceon@gmail.com>
2017-10-02 11:10:03 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,23 @@
-var express = require('express'); //init express
-var app = express(); //init app
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 var http = require('http').Server(app); //init server
 var io = require('socket.io')(http); //init socket.io
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(app.static("public"));
-app.use("/bootstrap/css", express.static("node_modules/bootstrap/dist/css"));
-app.use("/bootstrap/js", express.static("node_modules/bootstrap/dist/js"));
-app.use("/jq", express.static("node_modules/jquery/dist"));
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + "/chatbox.html");
-});
-
-io.on('connection', function(socket){
-  socket.emit("server message", "Please enter your username")
-  var usr;
-  socket.on("submission", function(msg){
-    var regex = /(<([^>]+)>)/ig;
-    msg = msg.replace(regex, "");
-    if(usr===undefined){
-      if(msg===""){
-        socket.emit("server message", "Invalid username. Please try again.");
-      }else{
-        usr = msg;
-        socket.emit("name set");
-        socket.emit("server message", "Welcome " + usr + "!");
-        socket.broadcast.emit("server message", "User "  + usr + " connected.");
-        console.log("user chose name " + usr);
-      }
-    }else{
-      io.emit("submission", usr, msg);
-    }
-  });
-  socket.on('disconnect', function(){
-    if(usr!==undefined){
-      io.emit("server message", usr + " has left.");
-    }
-  })
-});
-
-//begin listening
-http.listen(app.get('port'), function(){
-  console.log('listening on *:', app.get('port'));
-});
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+app.post('/', function (req, res) {
+  const msg=req.body.Body
+  //var regex = /[0-9]+ *ft|feet|foot|in|inch|yard|yd|mi|mile|pound|lb|ton|psi|atm/ig;
+  res.set('Content-Type', 'text/plain')
+  res.send(req.challenge)
+})
+ 
+// Tell our app to listen on port 3000
+app.listen(3000, function (err) {
+  if (err) {
+    throw err
+  }
+ 
+  console.log('Server started on port 3000')
+})
