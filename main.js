3a7fd3c4b8a9c936a59262660e72dc201c029786
TechNickL1 <auraceon@gmail.com>
2017-10-02 14:20:35 -0700

    test log update

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-  console.log(req.body.type)
-  console.log(req.body.text)
+  console.log("type= "+req.body.type)
+  console.log("content= "req.body.text)
   if(req.body.type="message"){
     var msg=req.body.text
     var regex = /[0-9]+ *ft|feet|foot|in|inch|yard|yd|mi|mile|pound|lb|ton|psi|atm/ig;
