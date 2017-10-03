9e1b649ba1d765168b8115f30ab5b8adcfeb328d
TechNickL1 <auraceon@gmail.com>
2017-10-02 18:15:58 -0700

    removed extranneous stuff, added flags

--- main.js
+++ main.js
@@ -19,19 +19,21 @@ app.post('/', function (req, res) {
     var msg=req.body.text;
     var regex = /.*[0-9]+\.*[0-9]* .+/ig;
     var params = msg.split(" ");
-    console.log("index of unit in possible units :"+(convert().possibilities().indexOf(params[1])));
     if(params[0] === "help"){
+      console.log("Help");
       res.send({"response_type":"ephemeral", "text":"Usage: /convert [value] [units from] [units to (optional)]\nPlease use spaces :)\nFor units with exponents, omit the ^. Only simple exponents (cm2 etc) are valid."});
     }else if(!regex.test(msg)){
+      console.log("invalid message, failed regex");
       res.send({"response_type":"ephemeral", "text":"Oops! Something went wrong. Try \"/convert help\" for help."});
     }else if(!(convert().possibilities().indexOf(params[1]) >= 0)){
+      console.log("first units invalid");
       res.send({"response_type":"ephemeral", "text":"Invalid units. Please use one of the following: " + convert().possibilities()});
     }else if(params.length >= 3 && convert().possibilities().indexOf(params[2]) >= 0){
+      console.log("has destination units, and theyre correct");
       var ans = convert(params[0]).from(params[1]).to(params[2]);
       res.send({"response_type":"in_channel", "text":params[0] + " " + params[1] + " = " + ans + " " + params[2]});
     }else if(params.length >= 3 && !(convert().possibilities().indexOf(params[2]) >= 0)){
-      res.send({"response_type":"ephemeral", "text":"Invalid units. Please use one of the following: " + convert().possibilities()});
-    }else if(!(convert().possibilities().indexOf(params[2]) >= 0)){
+      console.log("has destination units, and theyre incorrect");
       res.send({"response_type":"ephemeral", "text":"Invalid units. Please use one of the following: " + convert().possibilities()});
     }else{
       var ans = convert(params[0]).from(params[1]).toBest()
